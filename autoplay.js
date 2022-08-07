59dedf875c2b3c55141bc1ee38521c5efd7eaf09
darkwingsoulz <darkwingsoulz@gmail.com>
2022-08-06 21:46:57 -0500

    update default game loop from 24 hours to 25 hours

--- autoplay.js
+++ autoplay.js
@@ -47,7 +47,10 @@ const RECIPE_BARON = 1
 const RECIPE_CASTLE = 1
 const RECIPE_METAL = 3
 
-const MINT_TIMER = 24
+//we don't want to do exactly 24 hours because it could miss those about to expire
+//adding the extra hour will ensure this doesn't happen
+//this value is also has an override in the env file
+const MINT_TIMER = 25
 
 const LAND_CLAIM_FINE_WOOD_FEE = 16
 const TOKEN_FINEWOOD = "CFWTEMP"
